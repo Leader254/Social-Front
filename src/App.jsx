6180cfb32bb12fb5ec7b756e4933e37b412bd076
Leader254 <gatheru.samuel@students.kyu.ac.ke>
2023-07-19 08:02:12 +0300

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-/* eslint-disable react/prop-types */
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import Login from './Pages/Login/Login'
 import Register from './Pages/SignUp/SignUp'
-import { createBrowserRouter as Router, RouterProvider, Outlet, Navigate } from 'react-router-dom';
+import { createBrowserRouter as Router, RouterProvider, Outlet } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Profile from './Pages/Profile/Profile';
 import RightBar from './Components/rightBar/rightBar';
 import LeftBar from './Components/leftBar/leftBar';
-import { useContext } from 'react';
-import { AuthContext } from './Context/authContext';
-
 
 function App() {
-
-  const { currentuser } = useContext(AuthContext);
-
   const Layout = () => {
     return (
       <div className='theme-dark'>
@@ -25,29 +18,16 @@ function App() {
           <div style={{ flex: 6 }}>
             <Outlet />
           </div>
-
           <RightBar />
         </div>
       </div>
     )
   }
 
-  const ProtectedRoute = ({ children }) => {
-    if (!currentuser) {
-      return <Navigate to='/login' />
-    }
-    return children
-  }
-
-
   const router = Router([
     {
       path: '/',
-      element: (
-        <ProtectedRoute>
-          <Layout />
-        </ProtectedRoute>
-      ),
+      element: <Layout />,
       children: [
         {
           path: '/',
@@ -67,12 +47,15 @@ function App() {
       path: '/register',
       element: <Register />
     }
-  ])
+  ]);
+
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <RouterProvider router={router}>
+      <div className='app'>
+        <Outlet />
+      </div>
+    </RouterProvider>
   )
 }
 
-export default App
+export default App;
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,41 +1,32 @@
-import { useContext } from 'react';
 import '../../CSS/Login.css'
-import { Link } from 'react-router-dom'
-import { AuthContext } from '../../Context/authContext';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-
-    const {login} = useContext(AuthContext);
-
-    const handleLogin = () =>{
-        login();
-    }
-
   return (
     <div className='login'>
-        <div className="login-card">
-            <div className="login-left">
-                <h1>Ready to connect</h1>
-                <p>
-                    Are you ready to connect with your friends and family?
-                    Then you are at the right place.
-                </p>
-                <span>
-                    Don't have an account?
-                </span>
-                <Link to='/register'>
-                <button className='registerbtn'>Sign Up</button>
-                </Link>
-            </div>
-            <div className="login-right">
-                <h1>Login</h1>
-                <form>
-                    <input type="text" placeholder='Username' />
-                    <input type="password" placeholder='Password' />
-                    <button onClick={handleLogin} className='loginbtn'>Login</button>
-                </form>
-            </div>
+      <div className="login-card">
+        <div className="login-left">
+          <h1>Ready to connect</h1>
+          <p>
+            Are you ready to connect with your friends and family?
+            Then you are at the right place.
+          </p>
+          <span>
+            Don't have an account? <Link to="/home">Click here</Link> to explore.
+          </span>
+          <Link to='/register'>
+            <button className='registerbtn'>Sign Up</button>
+          </Link>
         </div>
+        <div className="login-right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder='Username' />
+            <input type="password" placeholder='Password' />
+            <button className='loginbtn'>Login</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
